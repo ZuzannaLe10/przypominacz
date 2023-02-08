0b6046e09e3b7dd3403560517058d1e26a080a6b
Michał Oślizło <117891509+Michal-Oslizlo17@users.noreply.github.com>
2023-02-08 11:17:10 +0100

    Modyfikacja kodu
    Dodano kolory w terminalu i trochę batch'a.

--- main.cpp
+++ main.cpp
@@ -24,29 +24,29 @@ Task task[200]; // 200 elementowa tablica struktur
 // funkcja dodająca
 void Dodaj()
 {
-  cout << "Podaj godzine powiadomienia (hh):" << endl;
+  cout << "[40;35mPodaj godzine powiadomienia ([40;36mhh[40;35m):[97m" << endl;
   cin >> task[i].czas_pow_godz;
 
-  cout << "Podaj minuty powiadomienia (mm):" << endl;
+  cout << "[40;35mPodaj minuty powiadomienia ([40;36mmm[40;35m):[97m" << endl;
   cin >> task[i].czas_pow_min;
 
-  cout << "Podaj date powiadomienia (dd-mm-yyyy):" << endl;
+  cout << "[40;35mPodaj date powiadomienia ([40;36mdd-mm-yyyy[40;35m):[97m" << endl;
   cin >> task[i].data_pow;
 
-  cout << "Podaj tre\230\206 powiadomienia: " << endl;
+  cout << "[40;35mPodaj tre\230\206 powiadomienia:[97m" << endl;
   cin >> task[i].tresc;
 
-  cout << "Podaj wa\276no\230\206 powiadomienia (gg-mm): " << endl;
+  cout << "[40;35mPodaj wa\276no\230\206 powiadomienia ([40;36mgg-mm[40;35m):[97m" << endl;
   cin >> task[i].czas_waz;
 
-  cout << "Podaj wa\276no\230\206 powiadomienia (dd-mm-yyyy): " << endl;
+  cout << "[40;35mPodaj wa\276no\230\206 powiadomienia ([40;36mdd-mm-yyyy[40;35m):[97m" << endl;
   cin >> task[i].data_waz;
 
   cin.ignore();
 
   i++;
 
-  cout << ". Wcisnij ENTER aby zako\344czy\206 dzia\210anie funkcji!";
+  cout << ". [40;36mWcisnij ENTER aby zako\344czy\206 dzia\210anie funkcji![97m";
   getchar();
 }
 
@@ -67,10 +67,10 @@ void Zobacz()
       cout << "_________________________________________________________________" << endl;
     }
     else
-      cout << "Nie ma podanego powiadomienia w bazie!!";
+      cout << "[40;36mNie ma podanego powiadomienia w bazie![97m";
   }
 
-  cout << "Nacisnij ENTER, aby zakon\344czy\206 dzia\210anie funkcji";
+  cout << "[40;36mNacisnij ENTER, aby zakon\344czy\206 dzia\210anie funkcji[97m";
 
   getchar();
 }
@@ -89,7 +89,7 @@ void Zapisz()
     plik << task[j].czas_pow_godz << ":" << task[j].czas_pow_min << " || " << task[j].data_pow << " || " << task[j].tresc << " || " << task[j].czas_waz << " || " << task[j].data_waz << endl;
   }
 
-  cout << "Dane zostaly zapisane!. Wcisnij ENTER, aby zakon\344czy\206 dzia\210anie funkcji";
+  cout << "[40;36mDane zostaly zapisane!. Wcisnij ENTER, aby zakon\344czy\206 dzia\210anie funkcji[97m";
 
   getchar();
 }
@@ -101,12 +101,12 @@ void Usun()
 
   if (i != 0)
   {
-    cout << "Podaj numer powiadomienia do usuni\251cia: ";
+    cout << "[40;36mPodaj numer powiadomienia do usuni\251cia:[97m";
     cin >> kogo;
     cin.ignore();
 
     if (kogo > i)
-      cout << "Operacja nie powiodla sie. Brak powiadomienia o takim numerze";
+      cout << "[40;31mOperacja nie powiodla sie. Brak powiadomienia o takim numerze[97m";
     else
     {
       for (short k = kogo; k < i; k++)
@@ -120,13 +120,13 @@ void Usun()
       }
       i--;
 
-      cout << "Operacja zakonczyla sie sukcesem! Powiadomienie o numerze " << kogo << " zosta\210o zmienione!";
+      cout << "[40;32mOperacja zakonczyla sie sukcesem![40;36m Powiadomienie o numerze " << kogo << "[40;36m zosta\210o zmienione![97m";
       Zapisz();
     }
   }
   else
   {
-    cout << "Nie ma powiadomienia w bazie! Wcisnij ENTER, aby zakon\344czy\206 dzia\210anie funkcji!";
+    cout << "[40;36mNie ma powiadomienia w bazie! Wcisnij ENTER, aby zakon\344czy\206 dzia\210anie funkcji![97m";
     getchar();
   }
 }
@@ -138,52 +138,53 @@ void Edytuj()
 
   if (i != 0)
   {
-    cout << "Podaj numer powiadomienia kt\242re chcesz zmieni\206: ";
+    cout << "[40;36mPodaj numer powiadomienia kt\242re chcesz zmieni\206: [97m";
     cin >> kogo;
     cin.ignore();
 
     if (kogo > i)
-      cout << "Operacja nie powiodla sie. Brak powiadomienia o takim numerze";
+      cout << "[40;31mOperacja nie powiodla sie. Brak powiadomienia o takim numerze[97m";
     else
     {
       // jest -1 bo numeracja powiadomien jest taka jak w wypisaniu. Natiomiast
       //  tablica zaczyna sie od zera.
-      cout << "Podaj godzine powiadomienia (hh):" << endl;
+      cout << "[40;36mPodaj godzine powiadomienia (hh):[97m" << endl;
       cin >> task[kogo - 1].czas_pow_godz; // jest -1 bo zakladam ze numeracja powiadomien jest taka jak w wypisaniu. Natiomiast
       //  tablica zaczyna sie od zera.
-      cout << "Podaj minuty powiadomienia (mm):" << endl;
+      cout << "[40;36mPodaj minuty powiadomienia (mm):[97m" << endl;
       cin >> task[kogo - 1].czas_pow_min;
-      cout << "Podaj date powiadomienia (dd-mm-yyyy): " << endl;
+      cout << "[40;36mPodaj date powiadomienia (dd-mm-yyyy):[97m" << endl;
       cin >> task[kogo - 1].data_pow;
-      cout << "Podaj tre\230\206 powiadomienia: " << endl;
+      cout << "[40;36mPodaj tre\230\206 powiadomienia:[97m" << endl;
       cin >> task[kogo - 1].tresc;
-      cout << "Podaj wa\276no\230\206 powiadomienia (gg-mm): " << endl;
+      cout << "[40;36mPodaj wa\276no\230\206 powiadomienia (gg-mm):[97m" << endl;
       cin >> task[kogo - 1].czas_waz;
-      cout << "Podaj wa\276no\230\206 powiadomienia (dd-mm-yyyy): " << endl;
+      cout << "[40;36mPodaj wa\276no\230\206 powiadomienia (dd-mm-yyyy):[97m" << endl;
       cin >> task[kogo - 1].data_waz;
 
-      cout << "Operacja zakonczy\210a si\251 sukcesem! Powiadomienie o numerze " << kogo << " zosta\210o zmienione!";
+      cout << "[40;32mOperacja zakonczy\210a si\251 sukcesem! Powiadomienie o numerze [97m" << kogo << " zosta\210o zmienione![97m";
       Zapisz();
     }
   }
   else
   {
-    cout << "Nie ma powiadomienia w bazie! Wcisnij ENTER, aby zakon\344czy\206 dzia\210anie funkcji!";
+    cout << "[40;33mNie ma powiadomienia w bazie! Wcisnij ENTER, aby zakon\344czy\206 dzia\210anie funkcji![97m";
     getchar();
   }
 }
 
 int main()
 {
+  system("title Przypominacz & color 0f & ping -n 2 0.0.0.0 >nul & cls & @chcp 65001 >nul & if %errorlevel% == 1 echo Error podczas włączania protokołu UTF-8, Error code: Error while switching mode on module '@chcp 65001'! & if %errorlevel% == 0 echo Uruchomiono protokół UTF-8");
   char wybor;
   do
   {
-    cout << "Co chcesz zrobi\206?" << endl;
-    cout << "1. Dodaj powiadomienie" << endl;
-    cout << "2. Wy\230wietl wszystkie powiadomienia" << endl;
-    cout << "3. Zapisz wprowadzone powiadomienia " << endl;
-    cout << "4. Usun\245\206 powiadomienie" << endl;
-    cout << "5. Edytuj powiadomienie" << endl
+    cout << "[40;35mCo chcesz zrobi\206?[97m" << endl;
+    cout << "[96m1. [40;36mDodaj powiadomienie[97m" << endl;
+    cout << "[96m2. [40;36mWy\230wietl wszystkie powiadomienia[97m" << endl;
+    cout << "[96m3. [40;36mZapisz wprowadzone powiadomienia[97m" << endl;
+    cout << "[96m4. [40;36mUsun\245\206 powiadomienie[97m" << endl;
+    cout << "[96m5. [40;36mEdytuj powiadomienie[97m" << endl
          << endl;
 
     wybor = getch();
@@ -216,23 +217,23 @@ int main()
     time_t now = time(nullptr);
     tm *time = localtime(&now);
 
-    cout << "Aktualny czas to: "
+    cout << "[96mAktualny czas to:[97m"
          << time->tm_hour << " godzin i "
          << time->tm_min << " minut" << endl;
 
     if ((task[j].czas_pow_godz == time->tm_hour) && (task[j].czas_pow_min == time->tm_min))
     {
-      cout << "Podany czas powiadomienia:" << task[j].czas_pow_godz << ":" << task[j].czas_pow_min << endl;
-      cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
-      cout << "--> Tresc powiadomienia: " << task[j].tresc << endl;
-      cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+      cout << "[40;36mPodany czas powiadomienia:[97m [ " << task[j].czas_pow_godz << ":" << task[j].czas_pow_min << " ]" << endl;
+      cout << "[40;37m+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[97m" << endl;
+      cout << "[40;36m--> Tresc powiadomienia:[97m " << task[j].tresc << endl;
+      cout << "[40;37m+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[97m" << endl;
 
       system("start Reminder.vbs");
     }
     else
     {
-      cout << "Podany czas powiadomienia:" << task[j].czas_pow_godz << ":" << task[j].czas_pow_min << endl;
-      cout << "Brak powiadomie\344 na podan\245 date!" << endl;
+      cout << "[40;36mPodany czas powiadomienia:[97m" << task[j].czas_pow_godz << ":" << task[j].czas_pow_min << endl;
+      cout << "[40;36mBrak powiadomie\344 na podan\245 date![97m" << endl;
     }
   }
   return 0;
